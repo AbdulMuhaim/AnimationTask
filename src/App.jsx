1d0b2c07c69ef61df759b88155711cfe463c15a8
Abdul Muhaim <muhaim25@gmail.com>
2024-06-15 22:24:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import AnimatedSection from './Components/AnimatedSection';
+
+function App() {
+
+  return (
+    <div className="App">
+      <main>
+        <AnimatedSection/>
+      </main>
+    </div>
+  );
+}
+
+export default App;
